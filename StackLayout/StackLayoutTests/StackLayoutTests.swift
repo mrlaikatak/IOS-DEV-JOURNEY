6fb32c95cd414a4f9c4b491eb2e00469547362ea
Yunus ACAR <yunsacr@gmail.com>
2025-05-14 21:49:37 +0300

    Stack Layout - Storyboard

---
+++ StackLayout/StackLayoutTests/StackLayoutTests.swift
@@ -0,0 +1,17 @@
+//
+//  StackLayoutTests.swift
+//  StackLayoutTests
+//
+//  Created by Yunus ACAR on 5.05.2025.
+//
+
+import Testing
+@testable import StackLayout
+
+struct StackLayoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ StackLayout/StackLayoutUITests/StackLayoutUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  StackLayoutUITestsLaunchTests.swift
+//  StackLayoutUITests
+//
+//  Created by Yunus ACAR on 5.05.2025.
+//
+
+import XCTest
+
+final class StackLayoutUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
