43dc7b44fb4ae90ba1c3470d7be0ab1e859adc0f
Yunus ACAR <yunsacr@gmail.com>
2025-05-14 21:47:21 +0300

    Objects With Code - Storyboard

---
+++ ObjectsWithCode/ObjectsWithCodeTests/ObjectsWithCodeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectsWithCodeTests.swift
+//  ObjectsWithCodeTests
+//
+//  Created by Yunus ACAR on 6.05.2025.
+//
+
+import Testing
+@testable import ObjectsWithCode
+
+struct ObjectsWithCodeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
